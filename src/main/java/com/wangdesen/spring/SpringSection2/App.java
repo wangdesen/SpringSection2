0c8746c2032721e7db4c15c7e41456687782eb80
wangdesen <desen000@gmail.com>
2016-04-13 21:24:08 +0800

    Update App.java

--- src/main/java/com/wangdesen/spring/SpringSection2/App.java
+++ src/main/java/com/wangdesen/spring/SpringSection2/App.java
@@ -1,13 +1,12 @@
 package com.wangdesen.spring.SpringSection2;
 
 /**
- * Hello world!
- *
+ * Hello Git!
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Git!" );
     }
 }
